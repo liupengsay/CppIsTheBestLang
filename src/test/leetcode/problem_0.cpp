24a2c1f03cb5511cfd6212b7c39770f590f69d13
liupengsay <liupengsay@foxmail.com>
2023-07-31 22:55:56 +0800

    自动格式化代码

--- src/test/leetcode/problem_0.cpp
+++ src/test/leetcode/problem_0.cpp
@@ -2,11 +2,12 @@
 #include <iostream>
 #include <vector>
 #include <unordered_map>
+
 using namespace std;
 
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
+    vector<int> twoSum(vector<int> &nums, int target) {
         unordered_map<int, int> hashtable;
         for (int i = 0; i < nums.size(); ++i) {
             auto it = hashtable.find(target - nums[i]);
@@ -18,8 +19,6 @@ public:
         return {};
     }
 };
-
-
 
 
 int main() {
