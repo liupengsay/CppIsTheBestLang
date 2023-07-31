e14ae67979841175b62e80229d33c0cda16c48b7
liupengsay <liupengsay@foxmail.com>
2023-07-31 22:52:02 +0800

    修改项目名称

---
+++ src/basis/binary_search.cpp
@@ -0,0 +1,3 @@
+//
+// Created by Administrator on 2023/7/31.
+//
---
+++ src/test/leetcode/problem_1.cpp
@@ -0,0 +1,36 @@
+
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> hashtable;
+        for (int i = 0; i < nums.size(); ++i) {
+            auto it = hashtable.find(target - nums[i]);
+            if (it != hashtable.end()) {
+                return {it->second, i};
+            }
+            hashtable[nums[i]] = i;
+        }
+        return {};
+    }
+};
+
+
+
+
+int main() {
+    vector<int> nums = {2, 7, 11, 15};
+    int target = 9;
+    Solution solution;
+    vector<int> result = solution.twoSum(nums, target);
+    if (result.size() == 2 && result[0] == 0 && result[1] == 1) {
+        cout << "Test case passed." << endl;
+    } else {
+        cout << "Test case failed." << endl;
+    }
+    return 0;
+}
