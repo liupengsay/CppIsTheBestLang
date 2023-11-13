364ae4e4da38cbadaa1361ac9fc364a3f40fbf68
liupengsay <liupengsay@foxmail.com>
2023-11-13 23:56:14 +0800

    tree array

--- src/data_structure/tree_array/example.cpp
+++ src/data_structure/tree_array/example.cpp
@@ -8,40 +8,37 @@
 class TestGeneral {
 public:
     static void PointAddRangeSumTest() {
+
         std::random_device rd;
         std::mt19937 gen(rd());
 
-        for (int i = 0; i < 10; i++) {
-            int ceil = std::uniform_int_distribution<>(10, 1000)(gen);
-            std::uniform_int_distribution<> numDist(-ceil, ceil);
+        for (int xx = 0; xx < 100; xx++) {
+            int ceil = std::uniform_int_distribution<int>(10, 1000)(gen);
             std::vector<int> nums(ceil);
-            for (int j = 0; j < ceil; j++) {
-                nums[j] = numDist(gen);
-            }
+            std::generate(nums.begin(), nums.end(),
+                          [&]() { return std::uniform_int_distribution<int>(-ceil, ceil)(gen); });
 
-            PointAddRangeSum treeArray(ceil);
-            treeArray.build(nums);
+            PointAddRangeSum tree_array(ceil);
+            tree_array.build(nums);
 
-            for (int j = 0; j < ceil; j++) {
-                int d = numDist(gen);
-                int index = std::uniform_int_distribution<>(0, ceil - 1)(gen);
-                nums[index] += d;
+            for (int _ = 0; _ < ceil; ++_) {
+                int d = std::uniform_int_distribution<int>(-ceil, ceil)(gen);
+                int i = std::uniform_int_distribution<int>(0, ceil - 1)(gen);
+                nums[i] += d;
+                tree_array.pointAdd(i + 1, d);
 
-                treeArray.pointAdd(index + 1, d);
+                int left = std::uniform_int_distribution<int>(0, ceil - 1)(gen);
+                int right = std::uniform_int_distribution<int>(left, ceil - 1)(gen);
 
-                int left = std::uniform_int_distribution<>(0, ceil - 1)(gen);
-                int right = std::uniform_int_distribution<>(left, ceil - 1)(gen);
-                int sumRange = 0;
-                for (int k = left; k <= right; k++) {
-                    sumRange += nums[k];
-                }
-                assert(sumRange == treeArray.rangeSum(left + 1, right + 1));
-                assert(nums == treeArray.get());
+                int expected_sum = std::accumulate(nums.begin() + left, nums.begin() + right + 1, 0);
+                int range_sum = tree_array.rangeSum(left + 1, right + 1);
+
+                assert(expected_sum == range_sum);
+                assert(nums == tree_array.get());
             }
         }
-    };
+    }
 
-public:
     static void PointAscendPreMaxTest() {
         std::random_device rd;
         std::mt19937 gen(rd());
@@ -50,7 +47,7 @@ public:
         std::uniform_int_distribution<> highDist(0, 10000);
 
         for (int initial: {std::numeric_limits<int>::min(), 0}) {
-            for (int xx = 0; xx < 10; xx++) {
+            for (int xx = 0; xx < 100; xx++) {
                 int n = randDist(gen);
                 int low = (initial == std::numeric_limits<int>::min()) ? -1000 : 0;
                 int high = 10000;
@@ -77,14 +74,13 @@ public:
                 }
             }
         }
-    };
+    }
 
-public:
     static void PointAscendRangeMaxTest() {
         std::random_device rd;
         std::mt19937 gen(rd());
         for (int initial: {std::numeric_limits<int>::min(), 0}) {
-            for (int xx = 0; xx < 10; xx++) {
+            for (int xx = 0; xx < 100; xx++) {
                 int n = std::uniform_int_distribution<>(10, 1000)(gen);
                 int low = (initial == std::numeric_limits<int>::min()) ? -1000 : 0;
                 int high = 10000;
@@ -106,12 +102,11 @@ public:
 
     }
 
-public:
     static void PointChangeRangeSumTest() {
         std::random_device rd;
         std::mt19937 gen(rd());
 
-        for (int xx = 0; xx < 10; xx++) {
+        for (int xx = 0; xx < 100; xx++) {
             int ceil = std::uniform_int_distribution<>(10, 1000)(gen);
             std::vector<int> nums(ceil);
             for (int i = 0; i < ceil; i++) {
@@ -140,14 +135,13 @@ public:
         }
     }
 
-public:
     static void PointDescendPreMinTest() {
         std::random_device rd;
         std::mt19937 gen(rd());
 
         const int inf = std::numeric_limits<int>::max();
         for (int initial: {inf, 10000}) {
-            for (int xx = 0; xx < 10; xx++) {
+            for (int xx = 0; xx < 100; xx++) {
                 int n = std::uniform_int_distribution<>(10, 1000)(gen);
                 int low = -10000;
                 int high = 10000;
@@ -176,7 +170,193 @@ public:
             }
         }
     }
+
+    static void PointDescendRangeMinTest() {
+        std::random_device rd;
+        std::mt19937 gen(rd());
+
+        const int inf = std::numeric_limits<int>::max();
+        for (int initial: {inf, 10000}) {
+            for (int xx = 0; xx < 100; xx++) {
+                int n = std::uniform_int_distribution<>(10, 1000)(gen);
+                int low = -10000;
+                int high = 10000;
+
+                PointDescendRangeMin treeArray(n, initial);
+                std::vector<int> nums(n, initial);
+
+                for (int _ = 0; _ < 100; _++) {
+                    int x = std::uniform_int_distribution<>(low, high)(gen);
+                    int i = std::uniform_int_distribution<>(0, n - 1)(gen);
+                    nums[i] = std::min(nums[i], x);
+                    treeArray.pointDescend(i + 1, x);
+
+                    int ll = std::uniform_int_distribution<>(0, n - 1)(gen);
+                    int rr = std::uniform_int_distribution<>(ll, n - 1)(gen);
+                    int expected = *std::min_element(nums.begin() + ll, nums.begin() + rr + 1);
+                    int result = treeArray.rangeMin(ll + 1, rr + 1);
+                    assert(result == expected);
+                }
+            }
+        }
+    }
+
+    static void PointXorRangeXorTest() {
+        std::random_device rd;
+        std::mt19937 gen(rd());
+
+        for (int xx = 0; xx < 100; xx++) {
+            int n = std::uniform_int_distribution<>(10, 1000)(gen);
+            int low = -10000;
+            int high = 10000;
+            std::vector<int> nums(n);
+            for (int i = 0; i < n; i++) {
+                nums[i] = std::uniform_int_distribution<>(low, high)(gen);
+            }
+
+            PointXorRangeXor treeArray(n);
+            treeArray.build(nums);
+
+            for (int _ = 0; _ < 100; _++) {
+                int x = std::uniform_int_distribution<>(low, high)(gen);
+                int i = std::uniform_int_distribution<>(0, n - 1)(gen);
+                nums[i] ^= x;
+                treeArray.pointXor(i + 1, x);
+                assert(treeArray.get() == nums);
+
+                int ll = std::uniform_int_distribution<>(0, n - 1)(gen);
+                int rr = std::uniform_int_distribution<>(ll, n - 1)(gen);
+                int res = 0;
+                for (int j = ll; j <= rr; j++) {
+                    res ^= nums[j];
+                }
+                assert(res == treeArray.rangeXor(ll + 1, rr + 1));
+            }
+        }
+    }
+
+    static void RangeAddRangeSumTest() {
+        std::random_device rd;
+        std::mt19937 gen(rd());
+
+        for (int xx = 0; xx < 100; xx++) {
+            for (int yy = 0; yy < 10; yy++) {
+                int n = std::uniform_int_distribution<>(10, 1000)(gen);
+                std::vector<int> nums(n);
+                for (int i = 0; i < n; i++) {
+                    nums[i] = std::uniform_int_distribution<>(-10000, 10000)(gen);
+                }
+
+                RangeAddRangeSum treeArray(n);
+                treeArray.build(nums);
+
+                for (int _ = 0; _ < 10; _++) {
+                    int x = std::uniform_int_distribution<>(-10000, 10000)(gen);
+                    int ll = std::uniform_int_distribution<>(0, n - 1)(gen);
+                    int rr = std::uniform_int_distribution<>(ll, n - 1)(gen);
+
+                    for (int j = ll; j <= rr; j++) {
+                        nums[j] += x;
+                    }
+                    treeArray.rangeAdd(ll + 1, rr + 1, x);
+                    assert(treeArray.get() == nums);
+
+                    ll = std::uniform_int_distribution<>(0, n - 1)(gen);
+                    rr = std::uniform_int_distribution<>(ll, n - 1)(gen);
+                    assert(treeArray.rangeSum(ll + 1, rr + 1) ==
+                           std::accumulate(nums.begin() + ll, nums.begin() + rr + 1, 0));
+                }
+            }
+        }
+    }
+
+    static void PointAddRangeSum2DTest() {
+        std::random_device rd;
+        std::mt19937 gen(rd());
+
+        int m = 100;
+        int n = 100;
+        int high = 100000;
+
+        PointAddRangeSum2D tree(m, n);
+        std::vector<std::vector<int>> grid(m, std::vector<int>(n));
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                grid[i][j] = std::uniform_int_distribution<>(-high, high)(gen);
+                tree.pointAdd(i + 1, j + 1, grid[i][j]);
+            }
+        }
+
+        for (int _ = 0; _ < m; _++) {
+            int row = std::uniform_int_distribution<>(0, m - 1)(gen);
+            int col = std::uniform_int_distribution<>(0, n - 1)(gen);
+            int x = std::uniform_int_distribution<>(-high, high)(gen);
+            grid[row][col] += x;
+            tree.pointAdd(row + 1, col + 1, x);
+
+            int x1 = std::uniform_int_distribution<>(0, m - 1)(gen);
+            int y1 = std::uniform_int_distribution<>(0, n - 1)(gen);
+            int x2 = std::uniform_int_distribution<>(x1, m - 1)(gen);
+            int y2 = std::uniform_int_distribution<>(y1, n - 1)(gen);
+
+            int sum = 0;
+            for (int i = x1; i <= x2; i++) {
+                for (int j = y1; j <= y2; j++) {
+                    sum += grid[i][j];
+                }
+            }
+
+            assert(tree.rangeSum(x1 + 1, y1 + 1, x2 + 1, y2 + 1) == sum);
+        }
+    }
+
+    static void RangeAddRangeSum2DTest() {
+        int m = 100;
+        int n = 100;
+        int high = 100000;
+        RangeAddRangeSum2D tree(m, n);
+        std::vector<std::vector<int>> grid(m, std::vector<int>(n));
+
+        std::random_device rd;
+        std::mt19937 gen(rd());
+
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                grid[i][j] = std::uniform_int_distribution<int>(-high, high)(gen);
+                tree.rangeAdd(i + 1, j + 1, i + 1, j + 1, grid[i][j]);
+            }
+        }
+
+        for (int i = 0; i < m; ++i) {
+            int x1 = std::uniform_int_distribution<int>(0, m - 1)(gen);
+            int y1 = std::uniform_int_distribution<int>(0, n - 1)(gen);
+            int x2 = std::uniform_int_distribution<int>(x1, m - 1)(gen);
+            int y2 = std::uniform_int_distribution<int>(y1, n - 1)(gen);
+            int x = std::uniform_int_distribution<int>(-high, high)(gen);
+
+            for (int row = x1; row <= x2; row++) {
+                for (int col = y1; col <= y2; col++) {
+                    grid[row][col] += x;
+                }
+            }
+
+            tree.rangeAdd(x1 + 1, y1 + 1, x2 + 1, y2 + 1, x);
+
+            x1 = std::uniform_int_distribution<int>(0, m - 1)(gen);
+            y1 = std::uniform_int_distribution<int>(0, n - 1)(gen);
+            x2 = std::uniform_int_distribution<int>(x1, m - 1)(gen);
+            y2 = std::uniform_int_distribution<int>(y1, n - 1)(gen);
+            int expected = 0;
+            for (int row = x1; row <= x2; row++) {
+                for (int col = y1; col <= y2; col++) {
+                    expected += grid[row][col];
+                }
+            }
+            assert(tree.rangeQuery(x1 + 1, y1 + 1, x2 + 1, y2 + 1) == expected);
+        }
+    }
 };
+
 
 int main() {
     TestGeneral::PointAddRangeSumTest();
@@ -184,5 +364,10 @@ int main() {
     TestGeneral::PointAscendRangeMaxTest();
     TestGeneral::PointChangeRangeSumTest();
     TestGeneral::PointDescendPreMinTest();
+    TestGeneral::PointDescendRangeMinTest();
+    TestGeneral::PointXorRangeXorTest();
+    TestGeneral::RangeAddRangeSumTest();
+    TestGeneral::PointAddRangeSum2DTest();
+    TestGeneral::RangeAddRangeSum2DTest();
     return 0;
-};
+}
--- src/data_structure/tree_array/template.cpp
+++ src/data_structure/tree_array/template.cpp
@@ -239,3 +239,268 @@ private:
     int initial;
     std::vector<int> t;
 };
+
+class PointDescendRangeMin {
+public:
+    explicit PointDescendRangeMin(int n, int initial = std::numeric_limits<int>::max())
+            : n(n), initial(initial), a(n + 1, initial), t(n + 1, initial) {}
+
+    static int lowestBit(int x) {
+        return x & -x;
+    }
+
+    static int min(int a, int b) {
+        return (a < b) ? a : b;
+    }
+
+    void pointDescend(int x, int k) {
+        assert(1 <= x && x <= n);
+        if (a[x] <= k) {
+            return;
+        }
+        a[x] = k;
+        while (x <= n) {
+            t[x] = min(t[x], k);
+            x += lowestBit(x);
+        }
+    }
+
+    int rangeMin(int left, int r) {
+        assert(1 <= left && left <= r && r <= n);
+        int minVal = initial;
+        while (r >= left) {
+            if (r - lowestBit(r) >= left - 1) {
+                minVal = min(minVal, t[r]);
+                r -= lowestBit(r);
+            } else {
+                minVal = min(minVal, a[r]);
+                r -= 1;
+            }
+        }
+        return minVal;
+    }
+
+private:
+    int n;
+    int initial;
+    std::vector<int> a;
+    std::vector<int> t;
+};
+
+
+class PointXorRangeXor {
+public:
+    explicit PointXorRangeXor(int n)
+            : n(n), t(n + 1, 0) {}
+
+    static int lowestBit(int i) {
+        return i & (-i);
+    }
+
+    void build(const std::vector<int> &nums) {
+        assert(nums.size() == n);
+        std::vector<int> pre(n + 1, 0);
+        for (int i = 0; i < n; i++) {
+            pre[i + 1] = pre[i] ^ nums[i];
+            t[i + 1] = pre[i + 1] ^ pre[i + 1 - lowestBit(i + 1)];
+        }
+    }
+
+    std::vector<int> get() {
+        std::vector<int> result;
+        for (int i = 0; i < n; i++) {
+            result.push_back(rangeXor(i + 1, i + 1));
+        }
+        return result;
+    }
+
+    void pointXor(int i, int val) {
+        assert(1 <= i && i <= n);
+        while (i <= n) {
+            t[i] ^= val;
+            i += lowestBit(i);
+        }
+    }
+
+    int rangeXor(int x, int y) {
+        assert(1 <= x && x <= y && y <= n);
+        return x > 1 ? preXor(y) ^ preXor(x - 1) : preXor(y);
+    }
+
+private:
+    int n;
+    std::vector<int> t;
+
+    int preXor(int i) {
+        assert(1 <= i && i <= n);
+        int val = 0;
+        while (i) {
+            val ^= t[i];
+            i -= lowestBit(i);
+        }
+        return val;
+    }
+
+};
+
+
+class RangeAddRangeSum {
+public:
+    explicit RangeAddRangeSum(int n)
+            : n(n), t1(n + 1, 0), t2(n + 1, 0) {}
+
+    static int lowestBit(int x) {
+        return x & (-x);
+    }
+
+    void add(int k, int v) {
+        int v1 = k * v;
+        while (k <= n) {
+            t1[k] += v;
+            t2[k] += v1;
+            k += lowestBit(k);
+        }
+    }
+
+    static int sum(const std::vector<int> &t, int k) {
+        int ret = 0;
+        while (k) {
+            ret += t[k];
+            k -= lowestBit(k);
+        }
+        return ret;
+    }
+
+    void build(const std::vector<int> &nums) {
+        assert(nums.size() == n);
+        for (int i = 0; i < n; i++) {
+            rangeAdd(i + 1, i + 1, nums[i]);
+        }
+    }
+
+    std::vector<int> get() {
+        std::vector<int> nums(n);
+        for (int i = 0; i < n; i++) {
+            nums[i] = rangeSum(i + 1, i + 1);
+        }
+        return nums;
+    }
+
+    void rangeAdd(int left, int right, int v) {
+        add(left, v);
+        add(right + 1, -v);
+    }
+
+    int rangeSum(int left, int right) {
+        int a = (right + 1) * sum(t1, right) - sum(t2, right);
+        int b = left * sum(t1, left - 1) - sum(t2, left - 1);
+        return a - b;
+    }
+
+private:
+    int n;
+    std::vector<int> t1;
+    std::vector<int> t2;
+};
+
+class PointAddRangeSum2D {
+public:
+    explicit PointAddRangeSum2D(int m, int n)
+            : m(m), n(n), tree(m + 1, std::vector<int>(n + 1, 0)) {}
+
+    void pointAdd(int x, int y, int val) {
+        int i = x;
+        while (i <= m) {
+            int j = y;
+            while (j <= n) {
+                tree[i][j] += val;
+                j += (j & -j);
+            }
+            i += (i & -i);
+        }
+    }
+
+    int query(int x, int y) {
+        int res = 0;
+        int i = x;
+        while (i > 0) {
+            int j = y;
+            while (j > 0) {
+                res += tree[i][j];
+                j -= (j & -j);
+            }
+            i -= (i & -i);
+        }
+        return res;
+    }
+
+    int rangeSum(int x1, int y1, int x2, int y2) {
+        return query(x2, y2) - query(x2, y1 - 1) - query(x1 - 1, y2) + query(x1 - 1, y1 - 1);
+    }
+
+private:
+    int m;
+    int n;
+    std::vector<std::vector<int>> tree;
+};
+
+
+class RangeAddRangeSum2D {
+public:
+    explicit RangeAddRangeSum2D(int m, int n) : m(m), n(n), t1(m + 1, std::vector<int>(n + 1)),
+                                                t2(m + 1, std::vector<int>(n + 1)),
+                                                t3(m + 1, std::vector<int>(n + 1)),
+                                                t4(m + 1, std::vector<int>(n + 1)) {}
+
+
+    void rangeAdd(int x1, int y1, int x2, int y2, int val) {
+        add(x1, y1, val);
+        add(x1, y2 + 1, -val);
+        add(x2 + 1, y1, -val);
+        add(x2 + 1, y2 + 1, val);
+    }
+
+
+    int rangeQuery(int x1, int y1, int x2, int y2) {
+        return query(x2, y2) - query(x2, y1 - 1) - query(x1 - 1, y2) + query(x1 - 1, y1 - 1);
+    }
+
+private:
+    int m;
+    int n;
+    std::vector<std::vector<int>> t1;
+    std::vector<std::vector<int>> t2;
+    std::vector<std::vector<int>> t3;
+    std::vector<std::vector<int>> t4;
+
+    void add(int x, int y, int val) {
+        int i = x;
+        while (i <= m) {
+            int j = y;
+            while (j <= n) {
+                t1[i][j] += val;
+                t2[i][j] += val * x;
+                t3[i][j] += val * y;
+                t4[i][j] += val * x * y;
+                j += (j & -j);
+            }
+            i += (i & -i);
+        }
+    }
+
+    int query(int x, int y) {
+        assert(0 <= x && x <= m && 0 <= y && y <= n);
+        int res = 0;
+        int i = x;
+        while (i > 0) {
+            int j = y;
+            while (j > 0) {
+                res += (x + 1) * (y + 1) * t1[i][j] - (y + 1) * t2[i][j] - (x + 1) * t3[i][j] + t4[i][j];
+                j -= (j & -j);
+            }
+            i -= (i & -i);
+        }
+        return res;
+    }
+
+};
