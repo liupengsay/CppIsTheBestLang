8bbbefd302d35898e9dad539c08856e361d9324e
liupengsay <liupengsay@foxmail.com>
2024-06-10 19:02:10 +0800

    add cf1984 problem

--- src/strings/kmp/template.cpp
+++ src/strings/kmp/template.cpp
@@ -1,56 +1,27 @@
-from typing import List
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
 
+using namespace std;
 
-class KMP:
-    def __init__(self):
-        return
-
-    @staticmethod
-    def prefix_function(s):
-        """calculate the longest common true prefix and true suffix for s [:i] and s [:i]"""
-        n = len(s)
-        pi = [0] * n
-        for i in range(1, n):
-            j = pi[i - 1]
-            while j > 0 and s[i] != s[j]:
-                j = pi[j - 1]
-            if s[i] == s[j]:
-                j += 1
-            pi[i] = j  # pi[i]<=i
-        # pi[0] = 0
-        return pi
-
-    @staticmethod
-    def z_function(s):
-        """calculate the longest common prefix between s[i:] and s"""
-        n = len(s)
-        z = [0] * n
-        left, r = 0, 0
-        for i in range(1, n):
-            if i <= r and z[i - left] < r - i + 1:
-                z[i] = z[i - left]
-            else:
-                z[i] = max(0, r - i + 1)
-                while i + z[i] < n and s[z[i]] == s[i + z[i]]:
-                    z[i] += 1
-            if i + z[i] - 1 > r:
-                left = i
-                r = i + z[i] - 1
-        # z[0] = 0
-        return z
-
-    def find(self, s1: str, s2: str) -> List[int]:
-        """find the index position of s2 in s1"""
-        n, m = len(s1), len(s2)
-        pi = self.prefix_function(s2 + "#" + s1)
-        ans = []
-        for i in range(m + 1, m + n + 1):
-            if pi[i] == m:
-                ans.append(i - m - m)
-        return ans
-
-    def find_longest_palindrome(self, s: str, pos="prefix") -> int:
-        """calculate the longest prefix and longest suffix palindrome substring"""
-        if pos == "prefix":
-            return self.prefix_function(s + "#" + s[::-1])[-1]
-        return self.prefix_function(s[::-1] + "#" + s)[-1]
+class KMP {
+public:
+    vector<int> z_function(const string& s) {
+        int n = s.size();
+        vector<int> z(n);
+        for (int i = 1, l = 0, r = 0; i < n; ++i) {
+            if (i <= r) {
+                z[i] = min(r - i + 1, z[i - l]);
+            }
+            while (i + z[i] < n && s[z[i]] == s[i + z[i]]) {
+                ++z[i];
+            }
+            if (i + z[i] - 1 > r) {
+                l = i;
+                r = i + z[i] - 1;
+            }
+        }
+        return z;
+    }
+};
