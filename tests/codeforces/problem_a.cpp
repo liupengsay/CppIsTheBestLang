166c3629f869179fa24059218ab078ee82c6b4d1
liupengsay <liupengsay@foxmail.com>
2025-10-01 16:09:23 +0800

    add cf daily problem

--- tests/codeforces/problem_a.cpp
+++ tests/codeforces/problem_a.cpp
@@ -1,31 +1,127 @@
 #include <iostream>
-#include <thread>
-#include <shared_mutex>
-#include <mutex>
+#include <vector>
+#include <unordered_map>
+#include <climits>
+#include <algorithm>
 
-std::shared_mutex shared_mtx;
-int shared_var = 0;
+using namespace std;
 
-void read_shared_var() {
-    std::shared_lock<std::shared_mutex> lock(shared_mtx);
-    std::cout << "Read shared_var: " << shared_var << "\n";
-}
+const int INF = 1e16;
 
-void write_shared_var(int x) {
-    std::unique_lock<std::shared_mutex> lock(shared_mtx);
-    shared_var = x;
-    std::cout << "Updated shared_var to: " << shared_var << "\n";
-}
+class PointSetRangeMaxSubSum {
+public:
+    int n;
+    vector<int> cover, left, right, sum;
 
-int main() {
-    std::thread t1(read_shared_var);
-    std::thread t2(write_shared_var, 2);
-    std::thread t4(write_shared_var, 4);
-    std::thread t3(read_shared_var);
+    PointSetRangeMaxSubSum(int n, int initial = INF) : n(n) {
+        cover.resize(4 * n, -initial);
+        left.resize(4 * n, -initial);
+        right.resize(4 * n, -initial);
+        sum.resize(4 * n, 0);
+    }
 
-    t1.join();
-    t2.join();
-    t3.join();
-    t4.join();
-    return 0;
-}
+    void build(const vector<int>& nums) {
+        vector<tuple<int, int, int>> stack;
+        stack.push_back({0, n - 1, 1});
+
+        while (!stack.empty()) {
+            auto [s, t, i] = stack.back();
+            stack.pop_back();
+
+            if (i >= 0) {
+                if (s == t) {
+                    _make_tag(i, nums[s]);
+                    continue;
+                }
+                stack.push_back({s, t, -i-1});
+                int m = s + (t - s) / 2;
+                stack.push_back({s, m, i << 1});
+                stack.push_back({m + 1, t, (i << 1) | 1});
+            } else {
+                i = -i-1;
+                _push_up(i);
+            }
+        }
+    }
+
+    void _make_tag(int i, int val) {
+        cover[i] = val;
+        left[i] = val;
+        right[i] = val;
+        sum[i] = val;
+    }
+
+    static vector<int> _range_merge_to_disjoint(const vector<int>& res1, const vector<int>& res2) {
+        vector<int> res(4, -INF);
+        res[0] = max(res1[0], res2[0]);
+        res[0] = max(res[0], res1[2] + res2[1]);
+        res[1] = max(res1[1], res1[3] + res2[1]);
+        res[2] = max(res2[2], res2[3] + res1[2]);
+        res[3] = res1[3] + res2[3];
+        return res;
+    }
+
+    void _push_up(int i) {
+        vector<int> res1 = {cover[i << 1], left[i << 1], right[i << 1], sum[i << 1]};
+        vector<int> res2 = {cover[(i << 1) | 1], left[(i << 1) | 1], right[(i << 1) | 1], sum[(i << 1) | 1]};
+        vector<int> merged = _range_merge_to_disjoint(res1, res2);
+        cover[i] = merged[0];
+        left[i] = merged[1];
+        right[i] = merged[2];
+        sum[i] = merged[3];
+    }
+
+    int point_set_range_max_sub_sum(int ind, int val) {
+        int s = 0, t = n - 1, i = 1;
+        while (true) {
+            if (s == t && s == ind) {
+                _make_tag(i, val);
+                break;
+            }
+            int m = s + (t - s) / 2;
+            if (ind <= m) {
+                t = m;
+                i = i << 1;
+            } else {
+                s = m+1;
+                i = (i << 1) | 1;
+            }
+        }
+        while (i > 1) {
+            i /= 2;
+            _push_up(i);
+        }
+        return cover[1];
+    }
+};
+
+class Solution {
+public:
+    int maxSubarraySum(vector<int>& nums) {
+        int n = nums.size();
+        PointSetRangeMaxSubSum tree(n);
+        tree.build(nums);
+
+        unordered_map<int, vector<int>> dct;
+        for (int i = 0; i < n; ++i) {
+            dct[nums[i]].push_back(i);
+        }
+
+        int ans = tree.point_set_range_max_sub_sum(0, nums[0]);
+        for (const auto& [x, indices] : dct) {
+            if (indices.size() < n) {
+                int cur = -1;
+                for (int i : indices) {
+                    cur = tree.point_set_range_max_sub_sum(i, 0);
+                }
+                if (cur > 0) {
+                    ans = max(ans, cur);
+                }
+                for (int i : indices) {
+                    tree.point_set_range_max_sub_sum(i, x);
+                }
+            }
+        }
+        return ans;
+    }
+};
--- tests/codeforces/problem_b.cpp
+++ tests/codeforces/problem_b.cpp
@@ -1,50 +1,107 @@
 #include <iostream>
-#include <thread>
-#include <shared_mutex>
-#include <string>
-#include <mutex>
+#include <vector>
+#include <set>
+#define int128 __int128_t
 
-std::shared_mutex shared_mtx;
-std::string shared_str;
-
-void write_shared_str(const std::string& str) {
-    std::unique_lock<std::shared_mutex> lock(shared_mtx);
-    shared_str = str;
-    std::cout << "Written: " << shared_str << "\n";
-}
-
-std::string read_shared_str() {
-    std::shared_lock<std::shared_mutex> lock(shared_mtx);
-    return shared_str;
+std::vector<long long> read_list_ints_minus_one(long long n) {
+    std::vector<long long> res(n);
+    for (long long i = 0; i < n; ++i) {
+        std::cin >> res[i];
+        --res[i];
+    }
+    return res;
 }
 
 int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
 
-    std::thread reader3([]() {
-        std::string result = read_shared_str();
-        std::cout << "Read: " << result << "\n";
-    });
+    long long n;
+    std::cin >> n;
 
-    reader3.join();
+    std::vector<std::vector<std::pair<long long, long long>>> dct(n);
+    for (long long _ = 0; _ < n - 1; ++_) {
+        std::vector<long long> vals = read_list_ints_minus_one(3);
+        long long i = vals[0], j = vals[1];
+        long long ll = vals[2] + 1;
+        dct[i].emplace_back(ll, j);
+        dct[j].emplace_back(ll, i);
+    }
 
-    std::thread writer1(write_shared_str, "Hello");
-    std::thread writer2(write_shared_str, "World");
-    writer1.join();
-    writer2.join();
+    std::vector<long long> dis(n, 0);
+    std::vector<long long> parent(n, -1);
+    std::vector<long long> stack(1, 0);
+    std::vector<long long> visit(n, 0);
 
-    std::thread reader1([]() {
-        std::string result = read_shared_str();
-        std::cout << "Read: " << result << "\n";
-    });
-    std::thread writer3(write_shared_str, "Check");
+    while (!stack.empty()) {
+        long long x = stack.back();
+        stack.pop_back();
+        for (const auto& p : dct[x]) {
+            long long y = p.second;
+            long long w = p.first;
+            if (y != parent[x]) {
+                parent[y] = x;
+                stack.push_back(y);
+                dis[y] = dis[x] + w;
+            }
+        }
+    }
 
-    std::thread reader2([]() {
-        std::string result = read_shared_str();
-        std::cout << "Read: " << result << "\n";
-    });
+    std::set<std::pair<long long, long long>> lst;
+    for (long long x = 1; x < n; ++x) {
+        lst.emplace(dis[x], x);
+    }
 
-    reader1.join();
-    reader2.join();
-    writer3.join();
+    long long ans = 0;
+
+    for (long long i = 0; i < n; ++i) {
+        if (lst.empty()) {
+            std::cout << ans << "\n";
+            continue;
+        }
+
+        auto va2l = *lst.rbegin();
+        lst.erase(va2l);
+        long long dd = va2l.first;
+        long long ii = va2l.second;
+        ans += dd * 2;
+
+        stack.clear();
+        visit[ii] = 1;
+        while (parent[ii] != -1 && !visit[parent[ii]]) {
+            long long j = parent[ii];
+            if (dis[j]) {
+                lst.erase({dis[j], j});
+            }
+            visit[j] = 1;
+            stack.push_back(j);
+            ii = j;
+        }
+
+        for (long long w: stack) {
+            dis[w] = 0;
+        }
+
+        while (!stack.empty()) {
+            long long x = stack.back();
+            stack.pop_back();
+            for (const auto& p : dct[x]) {
+                long long y = p.second;
+                long long ww = p.first;
+                if (!visit[y] && y != parent[x]) {
+                    if (dis[y]) {
+                        lst.erase({dis[y], y});
+                    }
+                    dis[y] = dis[x] + ww;
+                    stack.push_back(y);
+                    if (dis[y]) {
+                        lst.emplace(dis[y], y);
+                    }
+                }
+            }
+        }
+        std::cout << ans << "\n";
+    }
+
     return 0;
 }
